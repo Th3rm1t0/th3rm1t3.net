ab386a77651190b087845570d692cecc03ba53d9
Th3rm1t3 <80260010+Th3rm1t0@users.noreply.github.com>
2024-12-16 22:48:27 +0900

    言語変更ボタンを追加

--- frontend/src/assets/app/App.tsx
+++ frontend/src/assets/app/App.tsx
@@ -1,15 +1,36 @@
 import './App.css'
-import { Container } from '@mui/material'
+import { Box, Fab, Container } from '@mui/material'
 import { Profile } from './profile/Profile'
 import { useState } from 'react'
 
 export const App = () => {
-  const [lang, _] = useState<"ja"|"en">("ja")
+  const [lang, setLang] = useState<"ja"|"en">("ja")
+
+  const changeLang = () => {
+    if (lang === "ja") {
+      setLang("en")
+    } else {
+      setLang("ja")
+    }
+  }
+
   return (
     <>
       <Container>
         <Profile lang={lang}/>
       </Container>
+      <Box 
+        sx={{
+          position: 'fixed',
+          bottom: 30,
+          left: 30,
+          zIndex: 1000,
+        }}
+      >
+        <Fab color="primary" aria-label="home" onClick={changeLang}>
+          {lang}
+        </Fab>
+      </Box>
     </>
   )
 }
