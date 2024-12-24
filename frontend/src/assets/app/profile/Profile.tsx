016683aa3d1c234828e4486aa2fd97d1c2a7392f
Th3rm1t3 <80260010+Th3rm1t0@users.noreply.github.com>
2024-12-24 22:28:08 +0900

    少し修正

--- frontend/src/assets/app/profile/Profile.tsx
+++ frontend/src/assets/app/profile/Profile.tsx
@@ -2,7 +2,7 @@ export const Profile = (props: {readonly lang?: "ja" | "en"} = { lang: "ja" })  
     return (
     <div
     id="profile-card"
-    className="flex flex-col items-stretch px-6 py-10 border border-gray-300 bg-white rounded-lg max-w-4xl mx-auto shadow-md"
+    className="flex flex-col items-stretch px-6 py-10 border border-gray-300 bg-white rounded-lg shadow-md"
     >
         <div className="flex flex-wrap items-center">
             <div
@@ -17,23 +17,21 @@ export const Profile = (props: {readonly lang?: "ja" | "en"} = { lang: "ja" })  
             </div>
             <div className="w-full sm:w-3/4 text-left">
                 <h2 className="text-2xl font-bold">Th3rm1t3</h2>
-                <p className="text-base mt-2">
                 {props.lang === "ja" ? (
-                    <div>
+                    <p className="text-base mt-2">
                     Th3rm1t3 と書いて「テルミット」と読みます。<br />
                     バックエンドをメインとして細々と活動しているジュニアエンジニアです。<br />
                     Python, Golang, Java, SQL などによく触れています。<br />
                     オタクですが雑食なので、いろいろなジャンルをつまみ食いしています。<br />
-                    </div>
+                    </p>
                 ) : (
-                    <div>
+                    <p className="text-base mt-2">
                     My name is "Th3rm1t3," pronounced as "テルミット" (/ˈtɛr.mɪt/).<br />
                     I'm a junior engineer mainly working on backend development in a low-key way.<br />
                     I often work with Python, Golang, Java, and SQL.<br />
                     I'm a bit of an otaku, but I enjoy exploring a variety of genres without sticking to one.<br />
-                    </div>
+                    </p>
                 )}
-                </p>
             </div>
         </div>
         <div className="flex flex-wrap mt-8 mx-auto space-x-10 md:space-x-24">
