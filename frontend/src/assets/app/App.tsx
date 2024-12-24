b3f308d24610e28b2803626e04d8e962a3fcee18
Th3rm1t3 <80260010+Th3rm1t0@users.noreply.github.com>
2024-12-24 22:28:15 +0900

    少し修正

--- frontend/src/assets/app/App.tsx
+++ frontend/src/assets/app/App.tsx
@@ -1,9 +1,14 @@
+import { Link } from 'react-router-dom'
 import './App.css'
 import { Profile } from './profile/Profile'
 import { useState } from 'react'
 
 export const App = () => {
-  const [lang, setLang] = useState<"ja"|"en">("ja")
+  type attribute = "none" | "timeline" | "skill"
+  type languages = "ja" | "en"
+
+  const [lang, setLang] = useState<languages>("ja")
+
 
   const changeLang = () => {
     if (lang === "ja") {
@@ -28,10 +33,26 @@ export const App = () => {
     )
   }
 
+  const ChangeAttributeButton = (props: {attr: attribute}) => {
+    return (
+      <div className='grow py-8 border border-gray-300 bg-white rounded-lg shadow-md'>
+        <Link to={"./" + props.attr}>{props.attr}</Link>
+      </div>
+    )
+  }
+
   return (
     <>
-      <div className='container mx-auto'>
-        <Profile lang={lang} />
+      <div className='flex md:items-center md:justify-center h-screen'>
+        <div className='flex-col'>
+          <div className='md:w-[800px]'>
+            <Profile lang={lang}/>
+          </div>
+          <div className='flex flex-row px-10 mt-5'>
+            <ChangeAttributeButton attr='timeline'/>
+            <ChangeAttributeButton attr='skill'/>
+          </div>
+        </div>
       </div>
       <ChangeLangButton />
     </>
